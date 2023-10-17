b39ed662ab96201e8c66d97115f619b8aadbdfcf
Petre Rodan <2b4eda@subdimension.ro>
2023-10-17 13:41:49 +0300

    pcb back from production, pin reconfiguration

--- firmware/vfd/rtca_now.h
+++ firmware/vfd/rtca_now.h
@@ -2,11 +2,11 @@
 #ifndef __RTCA_NOW_H__
 #define __RTCA_NOW_H__
 
-#define COMPILE_YEAR 2022
-#define COMPILE_MON 4
-#define COMPILE_DAY 18
-#define COMPILE_DOW 1
-#define COMPILE_HOUR 11
+#define COMPILE_YEAR 2023
+#define COMPILE_MON 8
+#define COMPILE_DAY 30
+#define COMPILE_DOW 3
+#define COMPILE_HOUR 19
 #define COMPILE_MIN 35
 
 #endif
--- firmware/vfd/version.h
+++ firmware/vfd/version.h
@@ -1,11 +1,11 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-// compiled on Mon Apr 18 11:45:57 UTC 2022
+// compiled on Wed Aug 30 16:35:52 UTC 2023
 
 #define VER_MAJOR   1
 #define VER_MINOR   0
-#define BUILD 411
-#define COMMIT 10
+#define BUILD 412
+#define COMMIT 14
 
 #endif
