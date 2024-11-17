515f3411fa652c8d7484a942b5197a829d65ce9c
Petre Rodan <petre.rodan@subdimension.ro>
2024-11-17 08:06:02 +0200

    pin map update

--- firmware/vfd/rtca_now.h
+++ firmware/vfd/rtca_now.h
@@ -3,10 +3,10 @@
 #define __RTCA_NOW_H__
 
 #define COMPILE_YEAR 2023
-#define COMPILE_MON 8
-#define COMPILE_DAY 30
-#define COMPILE_DOW 3
-#define COMPILE_HOUR 19
-#define COMPILE_MIN 35
+#define COMPILE_MON 10
+#define COMPILE_DAY 17
+#define COMPILE_DOW 2
+#define COMPILE_HOUR 14
+#define COMPILE_MIN 23
 
 #endif
